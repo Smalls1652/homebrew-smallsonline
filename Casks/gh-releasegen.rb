8b97fe6ac2eaad8dd926af8a189f75726f98f7d5
Timothy Small <smalls.online@icloud.com>
2024-03-04 13:37:11 -0500

    Update gh-releasegen to v0.2.0 (#3)

--- Casks/gh-releasegen.rb
+++ Casks/gh-releasegen.rb
@@ -1,8 +1,8 @@
 cask "gh-releasegen" do
   arch arm: "arm64", intel: "x64"
-  version "0.1.1"
-  sha256 arm: "02f88110323bcdcc8ee87ee29a3a9c0e4d3332c0009c1e012d4505fdd0aad9dc",
-         intel: "17af73771409f1e2aacb57a8a8255412028eb516c8d0dd7d6271129e71ca4e71"
+  version "0.2.0"
+  sha256 arm: "6b98ed3ef3bcc986d6beaeb438f5f1a00dd71a0f9ed642a0dfc7f4de6785a134",
+         intel: "f563e74fa400dfc6679a6518306d505a3754b1d2ddc1269f1a3a69490b2b4378"
 
   url "https://github.com/Smalls1652/GitHubReleaseGen/releases/download/v#{version}/gh-releasegen_osx-#{arch}_v#{version}.zip"
   name "gh-releasegen"
